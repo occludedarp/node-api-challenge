f333f92e5587e234d21572eed9fdc839da34cb3b
Cameron Moore <cmrnmr@gmail.com>
2020-03-13 16:38:42 -0400

    finished assignmnet by building out actions

--- api/actionRouter.js
+++ api/actionRouter.js
@@ -3,32 +3,57 @@ const Actions = require('../data/helpers/actionModel.js')
 
 const router = express.Router();
 
-// get,
-// insert,
-// update,
-// remove,
-
-// id
-// project_id: required
-// description: required
-// notes: required
-// completed
-
-router.get('/', (req, res) => {
-  // Actions.get(req.body)
-  // .then()
+router.get('/:id', (req, res) => {
+  Actions.get(req.params.id)
+  .then(actions => {
+    if(actions){
+      res.status(200).json(actions)
+    } else {
+      res.status(400).json({"message": "the action with the specificed id could not be found"})
+    }
+  })
+  .catch(actions => {
+    res.status(500).json({"message": "error retrieving the action"})
+  })
 })
 
 router.post('/', (req, res) => {
-
+  Actions.insert(req.body)
+  .then(actions => {
+    res.status(200).json(actions)
+  })
+  .catch(error => {
+    res.status(400).json({"message": "error creating the actions"})
+  })
 })
 
 router.put('/:id', (req, res) => {
-  
+  const changes = req.body
+  Actions.update(req.params.id, changes)
+  .then(actions => {
+    if(actions){
+      res.status(200).json(actions)
+    } else {
+      res.status(400).json({"message": "the action by that id could not be found"})
+    }
+  })
+  .catch( error => {
+    res.status(500).json({"message": "error making the changes"})
+  })
 })
 
 router.delete('/:id', (req, res) => {
-  
+  Actions.remove(req.params.id)
+  .then(actions => {
+    if(actions > 0){
+      res.status(200).json(actions)
+    } else {
+      res.status(400).json({"message": "the action by that id could not be found"})
+    }
+  })
+  .catch( error => {
+    res.status(500).json({"message": "error removing the action"})
+  })
 })
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const server = express();
 server.use(express.json());
 
 server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
 
 server.get('/', (req, res) => {
   res.send(`
